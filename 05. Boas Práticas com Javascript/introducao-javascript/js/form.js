82242b34e224b3869d292bc72f905e6a7c5c1c50
CaioAndrade <caio.mk.andrade@gmail.com>
2024-08-09 01:03:35 -0300

    06. Validação de Formulários
    Atividade 2 finalizada: Exibindo mensagens de erro.

--- 05. Boas Práticas com Javascript/introducao-javascript/js/form.js
+++ 05. Boas Práticas com Javascript/introducao-javascript/js/form.js
@@ -8,6 +8,14 @@ botaoAdicionar.addEventListener("click",function(){
 
     var pacienteTr = montaTr(paciente);
 
+    var erro = validaPaciente(paciente);
+
+    if (erro.length > 0) {
+        var mensagemErro = document.querySelector("#mensagem-erro");
+        mensagemErro.textContent = erro;
+        return;
+    }
+
     // Adicionando o paciente na tabela
     var tabela = document.querySelector("#tabela-pacientes");
 
@@ -47,4 +55,14 @@ function montaTd(dado,classe) {
     td.classList.add(classe);
 
     return td;
+}
+
+function validaPaciente(paciente) {
+    var erros = [];
+
+    if (!validaPeso(paciente.peso)) erros.push("Peso é inválido!");
+
+    if (!validaAltura(paciente.altura)) erros.push("Altura é inválido!");
+
+    return erros;
 }
