f3c678f9158c82930149f57d5a5cd38225393785
CaioAndrade <caio.mk.andrade@gmail.com>
2024-08-10 18:30:28 -0300

    07. Remoção, Delegação e Animação
    Aula finalizada

--- 05. Boas Práticas com Javascript/introducao-javascript/js/form.js
+++ 05. Boas Práticas com Javascript/introducao-javascript/js/form.js
@@ -11,8 +11,7 @@ botaoAdicionar.addEventListener("click",function(){
     var erro = validaPaciente(paciente);
 
     if (erro.length > 0) {
-        var mensagemErro = document.querySelector("#mensagem-erro");
-        mensagemErro.textContent = erro;
+        exibeMensagensDeErro(erro);
         return;
     }
 
@@ -22,6 +21,8 @@ botaoAdicionar.addEventListener("click",function(){
     tabela.appendChild(pacienteTr);
 
     form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 });
 
 function obtemPacienteDoFormulario(form) {
@@ -60,9 +61,37 @@ function montaTd(dado,classe) {
 function validaPaciente(paciente) {
     var erros = [];
 
+    if (paciente.nome.length == 0) {
+        erros.push("O nome não pode ser em branco.")
+    }
+
+    if (paciente.gordura.length == 0) {
+        erros.push("A gordura não pode ser em branco.")
+    }
+
+    if (paciente.altura.length == 0) {
+        erros.push("A altura não pode ser em branco.")
+    }
+
+    if (paciente.peso.length == 0) {
+        erros.push("O peso não pode ser em branco.")
+    }
+
     if (!validaPeso(paciente.peso)) erros.push("Peso é inválido!");
 
     if (!validaAltura(paciente.altura)) erros.push("Altura é inválido!");
 
     return erros;
+}
+
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro){
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
 }
